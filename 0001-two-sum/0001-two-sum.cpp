ac27e76613c1ed3e957c95ffcd62efc25343dc0d
Hemang Ranga <82731266+Hemang-Ranga@users.noreply.github.com>
2026-06-05 13:00:27 +0530

    Time: 0 ms (100%), Space: 14.9 MB (18.51%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,13 +3,13 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size(), i=0;
         vector<int> ans;
-        map<int,int> m;
+        unordered_map<int,int> m;
         for(i=0; i<n; i++){
             if(m.find(target-nums[i]) == m.end()) m.insert({nums[i], i});
             else{
                 ans.push_back(m[target-nums[i]]);
                 ans.push_back(i);
-                break;
+                return ans;
             }
         }
         return ans;
