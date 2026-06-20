616124c7beee669dc9d326d38d2e2a66c9120c6c
Hemang Ranga <82731266+Hemang-Ranga@users.noreply.github.com>
2026-06-20 23:38:24 +0530

    Time: 0 ms (100%), Space: 12.7 MB (5.52%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -5,8 +5,12 @@ public:
         if(n==1) return 0;
         if(nums[0]>nums[1]) return 0;
         if(nums[n-1]>nums[n-2]) return n-1;
-        for(int i=1; i<n-1; i++){
-            if(nums[i]>nums[i+1] && nums[i]>nums[i-1]) return i;
+        int l=0, r=n-1;
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]) return mid;
+            else if(nums[mid]>nums[mid+1]) r=mid-1;
+            else if((nums[mid]<nums[mid+1])) l=mid+1;
         }
         return -1;
     }
