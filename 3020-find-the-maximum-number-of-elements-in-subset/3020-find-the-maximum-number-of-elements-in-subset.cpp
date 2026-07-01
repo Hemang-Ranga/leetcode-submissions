553ca4241ad9274b688915da0242d6bf6e6aef53
Hemang Ranga <82731266+Hemang-Ranga@users.noreply.github.com>
2026-07-01 18:06:33 +0530

    Time: 49 ms (95.33%), Space: 122.7 MB (57.9%) - LeetHub

--- 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.cpp
+++ 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.cpp
@@ -1,39 +1,28 @@
 class Solution {
 public:
     int maximumLength(vector<int>& nums) {
-        unordered_map<long long, int> mp;
-
-        for (int x : nums)
-            mp[x]++;
-
-        int ans = 1;
-
-        // Handle 1 separately
-        if (mp.count(1)) {
-            int cnt = mp[1];
-            ans = max(ans, cnt % 2 ? cnt : cnt - 1);
+        int ans=1;
+        unordered_map<long long, int> m;
+        for(int i: nums){
+            m[i]++;
         }
-
-        for (auto &[num, f] : mp) {
-            if (num == 1) continue;
-
-            long long x = num;
-            int len = 0;
-
-            while (mp.count(x) && mp[x] >= 2) {
-                len += 2;
-
-                if (x > 1e9) break; // avoid overflow
-                x = x * x;
+        //handle 1 separately
+        if(m.find(1) != m.end()){
+            if(m[1]%2==0) ans=m[1]-1;
+            else ans=m[1];
+        }
+        //taking each element as first
+        for(auto i: m){
+            long long curr = i.first;
+            if(curr==1) continue;
+            int len=0;
+            while(m.find(curr)!=m.end() && m[curr]>=2){
+                len+=2;
+                curr = curr*curr;
             }
-
-            if (mp.count(x))
-                len++;
-
-            else
-                len--;
-
-            ans = max(ans, len);
+            if(m.find(curr)!=m.end() && m[curr]==1) len++;
+            else len--;
+            ans = max(ans,len);
         }
         return ans;
     }
