ec8cc2757699f5bd280f75889c0cd67cb7c530ec
Hemang Ranga <82731266+Hemang-Ranga@users.noreply.github.com>
2026-07-03 22:11:35 +0530

    Time: 0 ms (100%), Space: 18.2 MB (45.56%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &img, vector<vector<int>> &ans, int iniclr, int newclr, int sr, int sc){
+    void bfs(int sr, int sc, vector<vector<int>> &img, vector<vector<int>> &ans, int newclr, int iniclr){
         int m = img.size(), n = img[0].size();
+        queue<pair<int,int>> q;
         int delrow[] = {-1,0,1,0};
         int delcol[] = {0,1,0,-1};
+        q.push({sr,sc});
         ans[sr][sc]=newclr;
-        for(int i=0; i<4; i++){
-            int nrow = sr + delrow[i];
-            int ncol = sc + delcol[i];
-            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && img[nrow][ncol]== iniclr && ans[nrow][ncol]!=newclr) dfs(img, ans, iniclr, newclr, nrow, ncol);
+        while(!q.empty()){
+            pair<int,int> p = q.front();
+            q.pop();
+            int orow = p.first, ocol=p.second;
+            for(int i=0; i<4; i++){
+                int nrow = orow+delrow[i];
+                int ncol = ocol+delcol[i];
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && img[nrow][ncol]==iniclr && ans[nrow][ncol]!=newclr){
+                    q.push({nrow, ncol});
+                    ans[nrow][ncol] = newclr;
+                }
+            }
         }
     }
 
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         vector<vector<int>> ans = image;
         int inicolor = image[sr][sc];
-        dfs(image,ans,inicolor,color,sr,sc);
+        bfs(sr,sc,image,ans,color,inicolor);
         return ans;
     }
 };
