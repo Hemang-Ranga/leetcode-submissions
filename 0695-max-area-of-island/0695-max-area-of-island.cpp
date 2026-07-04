536f26ede667e33736a82d781de75dabfc85aaf6
Hemang Ranga <82731266+Hemang-Ranga@users.noreply.github.com>
2026-07-04 20:27:33 +0530

    Time: 0 ms (100%), Space: 28.1 MB (58.28%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
-    int bfs(int row, int col, vector<vector<int>>& grid, vector<vector<bool>> &vis){
-        int m=grid.size(), n=grid[0].size(), area=0;
+    int dfs(int row, int col, vector<vector<int>>& grid, vector<vector<bool>> &vis){
+        int m=grid.size(), n=grid[0].size(), curr_area=0, remaining_area=0;
         int dr[] = {-1,0,1,0};
         int dc[] = {0,1,0,-1};
-        queue<pair<int,int>> q;
-        q.push({row,col});
-        vis[row][col] = true;
-        while(!q.empty()){
-            auto p = q.front();
-            q.pop();
-            area++;
-            int r=p.first, c=p.second;
-            for(int i=0; i<4; i++){
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-                if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]==1){
-                    if(!vis[nr][nc]){
-                        q.push({nr,nc});
-                        vis[nr][nc]=true;
-                    }
+        vis[row][col]=true;
+        curr_area++;
+        for(int i=0; i<4; i++){
+            int nr = row + dr[i];
+            int nc = col + dc[i];
+            if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]==1){
+                if(!vis[nr][nc]){
+                    remaining_area += dfs(nr,nc,grid,vis);
                 }
             }
         }
-        return area;
+        return curr_area+remaining_area;
     }
 
     int maxAreaOfIsland(vector<vector<int>>& grid) {
@@ -32,8 +24,8 @@ public:
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j]==1 && !visited[i][j]){
-                    int area = bfs(i,j,grid,visited);
-                    maxarea = max(maxarea, area);
+                    int area = dfs(i,j,grid,visited);
+                    maxarea = max(area, maxarea);
                 }
             }
         }
