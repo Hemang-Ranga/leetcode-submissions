32f8a778f3df403f26b9aec3b35a82bb02086d8a
Hemang Ranga <82731266+Hemang-Ranga@users.noreply.github.com>
2026-07-06 00:13:33 +0530

    Time: 26 ms (33.44%), Space: 25.1 MB (63.56%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -5,13 +5,12 @@ public:
         vector<vector<bool>> vis(n,vector<bool>(n,false));
         int dr[] = {0,1,1,1,0,-1,-1,-1};
         int dc[] = {1,1,0,-1,-1,-1,0,1};
-        queue<vector<int>> q;
+        queue<tuple<int,int,int>> q;
         q.push({row,col,1});
         vis[row][col] = true;
         while(!q.empty()){
-            auto u = q.front();
+            auto [r, c, dis] = q.front();
             q.pop();
-            int r = u[0], c = u[1], dis = u[2];
             if(r==n-1 && c==n-1) return dis;
             for(int i=0; i<8; i++){
                 int nr = r + dr[i];
@@ -28,6 +27,7 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
         if(grid[0][0]!=0 || grid[n-1][n-1]!=0) return -1;
+        if(n==1) return 1;
         return bfs(0,0,grid);
     }
 };
