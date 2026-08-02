2fa5e0b5c1896b39c4998ef6f11d6329f247fe9d
Hemang Ranga <82731266+Hemang-Ranga@users.noreply.github.com>
2026-08-02 16:54:48 +0530

    Time: 0 ms (100%), Space: 27.3 MB (16.1%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+        int p1 = 0, p2 = n - 1;
+        while (p1 < p2) {
+            swap(s[p1], s[p2]);
+            p1++;
+            p2--;
+        }
+    }
+};
