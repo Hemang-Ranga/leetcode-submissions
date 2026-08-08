5028774f0e0458b91cf36d1bcb3b791326fa4a88
Hemang Ranga <82731266+Hemang-Ranga@users.noreply.github.com>
2026-08-08 17:15:01 +0530

    Time: 12 ms (68.85%), Space: 18.7 MB (96.5%) - LeetHub

--- 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.cpp
+++ 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     vector<string> validStrings(int n) {
-        vector<string> ans, prev;
-        if(n==1){
-            ans.push_back("0");
-            ans.push_back("1");
-            return ans;
-        }
-        prev = validStrings(n-1);
-        for(string str: prev){
-            if(str.back()=='0'){
-                str.push_back('1');
-                ans.push_back(str);
-            }
-            else{
-                str.push_back('0');
-                ans.push_back(str);
-                str[str.size()-1]='1';
-                ans.push_back(str);
-            }
+        if (n == 1)
+            return {"0", "1"};
+        if (n == 2)
+            return {"01", "10", "11"};
+        vector<string> ans;
+        for (auto i : validStrings(n - 1)) {
+            ans.push_back(i + "1");
+            if (i[n - 2] == '1')
+                ans.push_back(i + "0");
         }
         return ans;
     }
