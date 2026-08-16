1ec9087450f4f1b86519045c509f69b71fa9588b
Hemang Ranga <82731266+Hemang-Ranga@users.noreply.github.com>
2026-08-17 04:45:19 +0530

    Time: 35 ms (12.93%), Space: 36 MB (59.65%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m=mat.size(), n=mat[0].size();
-        vector<vector<int>> dist(m,vector<int>(n,-1));
-        int dr[] = {-1,0,1,0};
-        int dc[] = {0,1,0,-1};
-        queue<tuple<int,int,int>> q;
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(mat[i][j]==0){
-                    q.push({i,j,0});
-                    dist[i][j] = 0;
+        int m = mat.size(), n = mat[0].size();
+        vector<vector<int>> ans(m, vector<int>(n, -1));
+        queue<tuple<int, int, int>> q;
+        int dr[] = {-1, 0, 1, 0};
+        int dc[] = {0, 1, 0, -1};
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    ans[i][j] = 0;
+                    q.push({i, j, 0});
                 }
             }
         }
-        while(!q.empty()){
-            auto [r,c,d] = q.front();
+
+        while (!q.empty()) {
+            auto [r, c, d] = q.front();
             q.pop();
-            for(int i=0; i<4; i++){
+            for (int i = 0; i < 4; i++) {
                 int nr = r + dr[i];
                 int nc = c + dc[i];
-                if(nr>=0 && nr<m && nc>=0 && nc<n && mat[nr][nc]==1 && dist[nr][nc]==-1){
-                    q.push({nr,nc,d+1});
-                    dist[nr][nc] = d+1;
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n &&
+                    mat[nr][nc] == 1 && ans[nr][nc] == -1) {
+                    q.push({nr, nc, d + 1});
+                    ans[nr][nc] = d + 1;
                 }
             }
         }
-        return dist;
+        return ans;
     }
 };
