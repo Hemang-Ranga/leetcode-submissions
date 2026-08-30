6659596cab6fdda230cd46b7b6256edab118955c
Hemang Ranga <82731266+Hemang-Ranga@users.noreply.github.com>
2026-08-30 21:50:25 +0530

    Time: 0 ms (100%), Space: 9.5 MB (5.22%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int f(int r, int c, vector<vector<int>>& dp) {
+        if (r < 0 || c < 0)
+            return 0;
+        if (r == 0 && c == 0)
+            return 1;
+        if (dp[r][c] != -1)
+            return dp[r][c];
+        return dp[r][c] = f(r - 1, c, dp) + f(r, c - 1, dp);
+    }
+
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return f(m - 1, n - 1, dp);
+    }
+};
