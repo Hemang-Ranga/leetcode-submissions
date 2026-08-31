8f065c6f2b10a9be9f07c79b6931c424c47df1fb
Hemang Ranga <82731266+Hemang-Ranga@users.noreply.github.com>
2026-09-01 02:40:14 +0530

    Time: 3 ms (13.16%), Space: 13.4 MB (25.82%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-    int f(int r, int c, vector<vector<int>>& triangle,
-          vector<vector<int>>& dp) {
-        int n = triangle.size();
-        if (r == n - 1)
-            return dp[r][c] = triangle[r][c];
-        if (dp[r][c] != INT_MIN)
-            return dp[r][c];
-        int a = f(r + 1, c, triangle, dp);
-        int b = f(r + 1, c + 1, triangle, dp);
-        return dp[r][c] = triangle[r][c] + min(a, b);
-    }
-
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
         if (n == 1)
             return triangle[0][0];
-        vector<vector<int>> dp(n);
-        for (int i = 0; i < n; i++) {
+        vector<vector<int>> dp = triangle;
+        for (int i = n - 2; i >= 0; i--) {
             for (int j = 0; j < i + 1; j++) {
-                dp[i].push_back(INT_MIN);
+                int mini = min(dp[i + 1][j], dp[i + 1][j + 1]);
+                dp[i][j] = triangle[i][j] + mini;
             }
         }
-        return f(0, 0, triangle, dp);
+        return dp[0][0];
     }
 };
