308ce46c5350e55201f0b389a2b9cf53512cb248
Hemang Ranga <82731266+Hemang-Ranga@users.noreply.github.com>
2026-09-01 02:47:14 +0530

    Time: 0 ms (100%), Space: 13.4 MB (25.82%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -4,13 +4,15 @@ public:
         int n = triangle.size();
         if (n == 1)
             return triangle[0][0];
-        vector<vector<int>> dp = triangle;
+        vector<int> prev = triangle[n - 1];
         for (int i = n - 2; i >= 0; i--) {
+            vector<int> curr(i + 1);
             for (int j = 0; j < i + 1; j++) {
-                int mini = min(dp[i + 1][j], dp[i + 1][j + 1]);
-                dp[i][j] = triangle[i][j] + mini;
+                int mini = min(prev[j], prev[j + 1]);
+                curr[j] = triangle[i][j] + mini;
             }
+            prev = curr;
         }
-        return dp[0][0];
+        return prev[0];
     }
 };
